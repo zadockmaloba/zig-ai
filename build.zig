80a5981399f274c653d50c37d4d8979802194f7c
Zadock Maloba <55388270+zadockmaloba@users.noreply.github.com>
2025-12-04 09:05:35 +0300

    Zig 0.15.2 compatibility

--- build.zig
+++ build.zig
@@ -6,13 +6,19 @@ pub fn build(b: *std.Build) void {
 
     const module = b.addModule("zig-ai", .{
         .root_source_file = b.path("src/llm.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const stream_example_mod = b.addModule("stream-cli", .{
+        .root_source_file = b.path("examples/stream-cli.zig"),
+        .target = target,
+        .optimize = optimize,
     });
 
     const stream_example = b.addExecutable(.{
         .name = "stream-cli",
-        .root_source_file = b.path("examples/stream-cli.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = stream_example_mod, 
     });
     stream_example.root_module.addImport("zig-ai", module);
 }
